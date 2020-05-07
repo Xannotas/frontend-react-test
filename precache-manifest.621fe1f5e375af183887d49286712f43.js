844f93c55259f0c781eebf02cd6f82a80c33b608
xannotas <xannotas@gmail.com>
2020-05-07 16:17:59 +0700

    Updates

---
+++ precache-manifest.621fe1f5e375af183887d49286712f43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e838c9857a8134e6682cbf81ebf321",
+    "url": "/frontend-react-test/index.html"
+  },
+  {
+    "revision": "53a323d29b7c1f845cfb",
+    "url": "/frontend-react-test/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "d3dffd3b33c80e51f0aa",
+    "url": "/frontend-react-test/static/css/main.72276ba4.chunk.css"
+  },
+  {
+    "revision": "53a323d29b7c1f845cfb",
+    "url": "/frontend-react-test/static/js/2.8b245bf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend-react-test/static/js/2.8b245bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3dffd3b33c80e51f0aa",
+    "url": "/frontend-react-test/static/js/main.82d8f6d7.chunk.js"
+  },
+  {
+    "revision": "63ec5854be5724953386",
+    "url": "/frontend-react-test/static/js/runtime-main.ba92a1c2.js"
+  }
+]);
